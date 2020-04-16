d98159769c2ce2b2756190dc76279cae164aab46
24-solar-terms <980327727@qq.com>
2020-04-16 20:33:53 +0800

    修改404，500错误处理函数

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,9 +4,9 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', title='Page Not Found'), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', title='Internal Server Error'), 500
